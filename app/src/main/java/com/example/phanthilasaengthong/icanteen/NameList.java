60eb0164ff74314956e7856a55d76b87a58415c8
mmoookkkk <phanthilamook@gmail.com>
2015-10-25 16:28:40 +0700

    hey

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
@@ -8,7 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class NameList extends AppCompatActivity {
- String[] food={"R.string.food1","R.string.food2","R.string.food3","R.string.food4","R.string.food5","R.string.food6","R.string.food7","R.string.food8","R.string.food9"};
+ String[] food={"R.string.food1","R.string.food2","R.string.food3","R.string.food4","R.string.food5","R.string.food6","R.string.food7","R.string.food8","R.string.food9","R.string.food10","R.string.food11"};
+    String[] dessert={"R.string.dessert1","R.string.dessert2","R.string.dessert3","R.string.dessert4"};
+    String[] beverage={"R.string.beverage1","R.string.beverage2","R.string.beverage3","R.string.beverage4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
