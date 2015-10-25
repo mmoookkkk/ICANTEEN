5d539d3caae83bc3e641de1c93ec92763ce8abea
Vicky <viki_vck@hotmail.com>
2015-10-25 13:44:17 +0700

    hey sed laew na

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
@@ -14,7 +14,7 @@ public class NameList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);///
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
