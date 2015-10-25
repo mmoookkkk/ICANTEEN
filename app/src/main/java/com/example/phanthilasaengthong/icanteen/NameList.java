2a5fc060117f206fd1a2c971a698945ca72245ed
mmoookkkk <phanthilamook@gmail.com>
2015-10-25 20:09:18 +0700

    hey

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
@@ -1,11 +1,14 @@
 package com.example.phanthilasaengthong.icanteen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NameList extends AppCompatActivity {
  String[] food={"R.string.food1","R.string.food2","R.string.food3","R.string.food4","R.string.food5","R.string.food6","R.string.food7","R.string.food8","R.string.food9","R.string.food10","R.string.food11"};
@@ -15,7 +18,10 @@ public class NameList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
-
+        Intent intent = getIntent();
+        String type = intent.getStringExtra(Choosing.TYPE_CHOSEN);
+        TextView type_title=(TextView)findViewById(R.id.type_title);
+        type_title.setText(type);
     }
 
 }
