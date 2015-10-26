ca75aaf0271770e9bea36dd1cb5ceeb9e7d440f5
mmoookkkk <phanthilamook@gmail.com>
2015-10-26 23:44:53 +0700

    try papss

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/NameList.java
@@ -1,12 +1,17 @@
 package com.example.phanthilasaengthong.icanteen;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+//import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,6 +27,50 @@ public class NameList extends AppCompatActivity {
         String type = intent.getStringExtra(Choosing.TYPE_CHOSEN);
         TextView type_title=(TextView)findViewById(R.id.type_title);
         type_title.setText(type);
+        PlaceholderFragment fragment1= new PlaceholderFragment();
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(R.id.fragment_container, fragment1).commit();
+
+
+    }
+    protected void onResume(){
+        super.onResume();
+
+
+
+
+
+    }
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            Intent context=getActivity().getIntent();
+            String type = context.getStringExtra(Choosing.TYPE_CHOSEN);
+            System.out.print(type);
+
+
+
+
+            if(type.equals(getResources().getString(R.string.FOOD))){
+                return inflater.inflate(R.layout.fragment_foodlist,
+                        container, false);
+
+            }else if(type.equals(getResources().getString(R.string.DESSERT))){
+               return inflater.inflate(R.layout.fragment_dessertlist,
+                        container, false);
+
+            }else{
+                return inflater.inflate(R.layout.fragment_beveragelist,
+                        container, false);
+            }
+
+
+        }
     }
 
 }
