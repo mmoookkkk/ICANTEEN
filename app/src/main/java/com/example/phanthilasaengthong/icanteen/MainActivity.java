39ab29929b1a3cd589d60c9f7f8ad54ee77731a2
mmoookkkk <phanthilamook@gmail.com>
2015-10-31 15:35:22 +0700

    try papss

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/MainActivity.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+       // ParseObject testObject = new ParseObject("TestObject");
+       // testObject.put("foo", "bar");
+       // testObject.saveInBackground();
         //addchange!!
     }
     public void clickChoose(View view) {
