ee3d0c13667c1e3c9b9ec02257ae4eb85e082eb4
Vicky <viki_vck@hotmail.com>
2015-10-31 15:14:54 +0700

    yayyyyyyyy

--- app/src/main/java/com/example/phanthilasaengthong/icanteen/Random.java
+++ app/src/main/java/com/example/phanthilasaengthong/icanteen/Random.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Random extends AppCompatActivity {
 
@@ -22,8 +23,61 @@ public class Random extends AppCompatActivity {
         super.onResume();
         int r=(int) Math.ceil(Math.random()*7);
         if(r==1){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("ข้าวเหนียวไก่ทอด");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food5));
 
         }
+
+        if(r==2){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("ข้าวไก่แซ่บ");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food6));
+
+        }
+
+        if(r==3){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("ไข่กระทะ");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food2));
+
+        }
+
+        if(r==4){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("ก๋วยเตี๋ยวต้มยำ");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food3));
+
+        }
+
+        if(r==5){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("ข้าวไก่กระเพรา");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food6));
+
+        }
+
+        if(r==6){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("หมูมะนาว");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.food3));
+
+        }
+
+        if(r==7){
+            TextView menu_random = (TextView) findViewById(R.id.menu_random);
+            menu_random.setText("parabola");
+            TextView res_random = (TextView) findViewById(R.id.random_shop);
+            res_random.setText(getResources().getText(R.string.dessert1));
+
+        }
+
 
 
 
